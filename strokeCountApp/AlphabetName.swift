c091b39a78221c97e1447862e9ce2fa6cab83df5
NR <motorbicykle@gmail.com>
2020-12-17 22:09:38 +0900

    initialize App

---
+++ strokeCountApp/AlphabetName.swift
@@ -0,0 +1,20 @@
+//
+//  AlphabetName.swift
+//  strokeCountApp
+//
+//  Created by NR on 2020/12/16.
+//  Copyright © 2020 nararyo. All rights reserved.
+//
+
+import Foundation
+
+class AlphabetName {
+    
+    var strokeCount: Int = 0
+    var rate: Double = 0
+    var describe: String = ""
+    
+    init() {
+        
+    }
+}

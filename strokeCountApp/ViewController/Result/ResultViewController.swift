0b1f7483aa645ba707bdaf55cf277d4f5167a40d
NR <motorbicykle@gmail.com>
2020-12-21 16:51:31 +0900

    ResultVCにscrollView追加

--- strokeCountApp/ViewController/Result/ResultViewController.swift
+++ strokeCountApp/ViewController/Result/ResultViewController.swift
@@ -16,12 +16,18 @@ class ResultViewController: UIViewController {
     var name: String?
     
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var contentView: UIView!
     
     @IBOutlet weak var countLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var cosmosView: CosmosView!
     @IBOutlet weak var describeLabel: UILabel!
     
+    
+    @IBOutlet weak var backButton: UIView!
+    @IBOutlet weak var saveButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,39 +45,62 @@ extension ResultViewController {
     
     func setViewPosition(){
         
-//        let tabHeight = tabBarController?.tabBar.frame.size.height
-//        let navHeight = navigationController?.navigationBar.frame.size.height
-//        let height = self.view.bounds.height - (tabHeight! + navHeight!)
-//        print(self.view.frame.height)
-//        print(height)
-//        print(tabHeight)
-//        print(navHeight)
-//        print(self.view.snp.top)
+        print(self.view.bounds.height)
+        print(UIScreen.main.bounds.height)
+        
+        
+        scrollView.snp.makeConstraints{(make) -> Void in
+            make.edges.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints{(make) -> Void in
+            make.width.equalTo(scrollView.frameLayoutGuide)
+            make.edges.equalTo(scrollView.contentLayoutGuide)
+            make.height.equalTo(self.view.bounds.height * 1.3)
+        }
         
         nameLabel.snp.makeConstraints{(make)-> Void in
-            make.top.equalTo(self.view.snp.top).offset(self.view.bounds.height / 5)
+            make.top.equalToSuperview().offset(self.view.frame.height / 9)
+            make.centerX.equalToSuperview()
         }
         
         countLabel.snp.makeConstraints{(make) -> Void in
             make.top.equalTo(nameLabel.snp.bottom).offset(108)
+            
+            make.centerX.equalToSuperview()
         }
         cosmosView.snp.makeConstraints{(make) -> Void in
             make.top.equalTo(self.countLabel.snp.bottom).offset(36)
+            make.centerX.equalToSuperview()
         }
         describeLabel.snp.makeConstraints{(make) -> Void in
             make.top.equalTo(self.cosmosView.snp.bottom).offset(36)
             make.right.left.equalToSuperview().inset(16)
+            make.centerX.equalToSuperview()
         }
+        
+//        backButton.snp.makeConstraints{(make) -> Void in
+//            make.bottom.equalTo(countLabel.snp.bottom).offset(208)
+//
+//        }
+        
+        print(contentView.bounds.height)
     }
     
     func setAnimation(){
         cosmosView.alpha = 0.0
         countLabel.alpha = 0.0
         describeLabel.alpha = 0.0
+        //backButton.alpha = 0.0
+        //saveButton.alpha = 0.0
+        
         UIView.animate(withDuration: 2.0, delay: 0.6, options: [.curveEaseIn], animations: {
             self.cosmosView.alpha = 1.0
             self.countLabel.alpha = 1.0
             self.describeLabel.alpha = 1.0
+            //self.backButton.alpha = 1.0
+            //self.saveButton.alpha = 1.0
+            
         }, completion: nil)
     }
 }
